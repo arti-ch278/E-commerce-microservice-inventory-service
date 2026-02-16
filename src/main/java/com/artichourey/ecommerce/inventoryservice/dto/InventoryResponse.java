f5a1880b0d970bd43431a928937ccebbcca29778
Arti <arti.chourey21@gmail.com>
2026-02-16 18:18:00 +0530

    feat: add inventory request/response DTOs

---
+++ src/main/java/com/artichourey/ecommerce/inventoryservice/dto/InventoryResponse.java
@@ -0,0 +1,16 @@
+package com.artichourey.ecommerce.inventoryservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class InventoryResponse {
+	
+	private String skuCode;
+	private boolean inStock;
+	private Integer availableQuantity;
+
+}
