d401cd217b2a2318c11af7a6e2cdaa56d0625094
Arti <arti.chourey21@gmail.com>
2026-03-29 15:02:31 +0530

    feat(dto): update InventoryRequest DTO to align with new business requirements

--- src/main/java/com/artichourey/ecommerce/inventoryservice/dto/InventoryRequest.java
+++ src/main/java/com/artichourey/ecommerce/inventoryservice/dto/InventoryRequest.java
@@ -1,7 +1,6 @@
 package com.artichourey.ecommerce.inventoryservice.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.persistence.Entity;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
