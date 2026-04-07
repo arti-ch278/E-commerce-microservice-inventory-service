bbedfbc3bd50df7cdc64e9000c16074461195b45
Arti <arti.chourey21@gmail.com>
2026-04-07 22:49:28 +0530

    feat: update inventory request DTO with new fields and validation

--- src/main/java/com/artichourey/ecommerce/inventoryservice/dto/InventoryRequest.java
+++ src/main/java/com/artichourey/ecommerce/inventoryservice/dto/InventoryRequest.java
@@ -3,6 +3,7 @@ package com.artichourey.ecommerce.inventoryservice.dto;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,16 +14,17 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Schema(description = "Request body for adding or updating inventory")
 public class InventoryRequest {
-	
-	@Schema(description = "Inventory ID (optional for new inventory)", example = "1")
-	private long id;
-	
-	@NotBlank(message="sku code must be not blank")
-	@Schema(description = "SKU code of the product", example = "SKU12345", required = true)
-	private String skuCode;
-	
-	@Min(value=0,message="Quantity must be 0 or greater")
-	@Schema(description = "Quantity of the product in inventory", example = "10", required = true)
-	private int quantity;
 
+    
+//    @Schema(description = "Inventory ID (optional for new inventory)", example = "1")
+//    private Long id;
+
+    @NotBlank(message = "sku code must not be blank")
+    @Schema(description = "SKU code of the product", example = "SKU12345", required = true)
+    private String skuCode;
+
+    @NotNull(message = "Quantity must not be null") 
+    @Min(value = 0, message = "Quantity must be 0 or greater")
+    @Schema(description = "Quantity of the product in inventory", example = "10", required = true)
+    private Integer quantity;
 }
