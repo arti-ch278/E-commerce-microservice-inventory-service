2a1fe50c852cd6534a3f9c20a71cea8c5f254b56
Arti <arti.chourey21@gmail.com>
2026-04-16 21:24:39 +0530

    feat(tracing): add tracing support to Kafka consumer configuration

--- src/main/java/com/artichourey/ecommerce/inventoryservice/config/KafkaConsumerConfig.java
+++ src/main/java/com/artichourey/ecommerce/inventoryservice/config/KafkaConsumerConfig.java
@@ -1,11 +1,16 @@
 package com.artichourey.ecommerce.inventoryservice.config;
 
+import org.apache.kafka.common.TopicPartition;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.listener.DeadLetterPublishingRecoverer;
 import org.springframework.kafka.listener.DefaultErrorHandler;
 import org.springframework.util.backoff.FixedBackOff;
+
+import io.micrometer.observation.ObservationRegistry;
 import lombok.extern.slf4j.Slf4j;
 
 @Configuration
@@ -14,16 +19,28 @@ public class KafkaConsumerConfig {
 
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, Object> kafkaListenerContainerFactory(
-            ConsumerFactory<String, Object> consumerFactory) {
+            ConsumerFactory<String, Object> consumerFactory,
+            ObservationRegistry observationRegistry) {
 
         ConcurrentKafkaListenerContainerFactory<String, Object> factory =
                 new ConcurrentKafkaListenerContainerFactory<>();
+
         factory.setConsumerFactory(consumerFactory);
 
-        //  Modern retry + error handling
-        DefaultErrorHandler errorHandler = new DefaultErrorHandler(
-                new FixedBackOff(2000L, 3) // 3 retries, 2 sec delay
-        );
+        // Enable tracing
+        factory.getContainerProperties().setObservationEnabled(true);
+        factory.getContainerProperties().setObservationRegistry(observationRegistry);
+
+        // Retry: 3 attempts with 2 sec delay
+        FixedBackOff fixedBackOff = new FixedBackOff(2000L, 3);
+
+        // No DLT - just retry + fail after retries exhausted
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler(fixedBackOff);
+
+        errorHandler.setRetryListeners((record, ex, deliveryAttempt) -> {
+            log.warn("Inventory Retry | topic={} | partition={} | attempt={}",
+                    record.topic(), record.partition(), deliveryAttempt);
+        });
 
         factory.setCommonErrorHandler(errorHandler);
 
